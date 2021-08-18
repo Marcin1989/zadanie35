c032e00e41b5a935b11cec72d0110f34f097ff2f
marcin1989 <marcinlubanski23@gmail.com>
2021-08-18 07:43:41 +0200

    poprawa nazwy zmiennej

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-let cos = document.querySelector("p");
+let paragraf = document.querySelector("p");
 
-console.log(cos);{
-    cos.textContent = "pierwszy teskt w JavaScript"
-};
+console.log(paragraf);
+
+paragraf.textContent = "pierwszy teskt w JavaScript";
 
 
 
